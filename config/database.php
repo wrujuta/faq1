92b997dee25bee142502a463d52499bf87ed56a8
wrujuta <wpd2@njit.edu>
2019-05-12 20:07:02 -0400

    Added Procfile

--- config/database.php
+++ config/database.php
@@ -32,6 +32,16 @@ return [
     */
 
     'connections' => [
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -81,17 +91,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-        'pg-heroku' => [
-            'driver' => 'pgsql',
-            'host' => $heroku_db_url['host'],
-            'database' => substr($heroku_db_url['path'], 1),
-            'username' => $heroku_db_url['user'],
-            'password' => $heroku_db_url['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
-
 
 
     ],
